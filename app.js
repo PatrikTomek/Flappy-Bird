8f614abfc0dc0c382d1d11e375167187ad4095b6
Patrik Tomek <pataz23@seznam.cz>
2022-03-17 19:07:06 +0100

    Bird control

--- app.js
+++ app.js
@@ -3,10 +3,27 @@ const gameDisplay = document.querySelector(".game-container");
 
 let birdLeft = 220;
 let birdBottom = 200;
+let gravity = 2;
 
 const startGame = () => {
+  birdBottom -= gravity;
   bird.style.bottom = birdBottom + "px";
   bird.style.left = birdLeft + "px";
 };
 
-startGame();
+let timerId = setInterval(startGame, 20);
+
+const control = (e) => {
+  if (e.keyCode === 32) {
+    jump();
+  }
+};
+
+const jump = () => {
+  if (birdBottom < 640) {
+    birdBottom += 50;
+    bird.style.bottom = birdBottom + "px";
+  }
+};
+
+addEventListener("keyup", control);
